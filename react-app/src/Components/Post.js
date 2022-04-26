08750cfbc0e7f6be4b78068f32ba859ce717abdd
swang1212 <99310286+swang1212@users.noreply.github.com>
2022-04-25 18:13:46 -0700

    Update Post.js

--- react-app/src/Components/Post.js
+++ react-app/src/Components/Post.js
@@ -1,9 +1,14 @@
-const Post = ({ title, body }) => {
+import Comments from "./Comments";
+import NewComment from "./NewComment";
+
+const Post = ({ title, body, ID, comments }) => {
   return <div style={{ textAlign: 'left', marginBottom: '12px'}}>
     <h3>
       {title}
     </h3>
     <p>{body}</p>
+    <Comments body = {comments}></Comments>
+    <NewComment id = {ID}/>
   </div>
 }
 
